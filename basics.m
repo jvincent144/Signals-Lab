e9a904557f7da804c271e5c6ae83095d3b3a49b4
Joshua Vincent <jvincent@scu.edu>
2021-01-25 14:54:22 -0800

    Printed outputs

--- basics.m
+++ basics.m
@@ -53,33 +53,41 @@ title("Single Sample")
 a = [10 -13];
 b = [-5 3];
 [th, r] = cart2pol(a, b); % Cartesian to polar
+fprintf("Theta: %f\tR: %f\n", th, r)
 [u, v] = pol2cart(th, r); % Polar to cartesian
+fprintf("u: %f\tv: %f\n", u, v)
 
 % It is more convenient to perform calculations in polar space
 a = [8 3];
 b = [5 -6];
 [th, r] = cart2pol(a, b); % Cartesian to polar
+fprintf("Theta: %f\tR: %f\n", th, r)
 z = r.*exp(1j*th);
+fprintf("z(1): %f\tz(2): %f\n", z(1), z(2))
 product = z(1)*z(2);
 quotient = z(1)/z(2);
 power = z(1)^3;
+fprintf("Product: %f\tQuotient: %f\tPower: %f\n", product, quotient, power)
 
 % Addition of complex numbers in cartesian space
 a = [5 3];
 b = [5 4];
 z = complex(a, b);
 sum = z(1) + z(2);
+fprintf("Sum: %f\n", sum)
 
 % Addition of complex numbers in polar space
 th = [pi/6 pi/5];
 r = [2 10];
 z = r.*exp(1j*th);
 sum = z(1) + z(2);
+fprintf("Sum: %f\n", sum)
 
 % Condense multiple sinusoids into a single sinusoids
 a = 5;
 b = -1;
 [c, th] = condense(a, b);
+fprintf("c: %f\ttheta: %f\n", c, th)
 
 % Plotting sinusoids and exponentially-varying sinusoids
 t = 0:dt:50;
